0ebc0a20812c715262580af3cbc13ca71d21e5da
Cynthia Pinedo <cynthiapinedoh79@gmail.com>
2025-09-26 23:46:54 +0000

    adding const for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-yFJynNUfyQL9Bj5r98e1sJfhp-w
+const API_KEY = "yFJynNUfyQL9Bj5r98e1sJfhp-w";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultModal = new bootstrap.Modal(document.getElementById("resultModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
